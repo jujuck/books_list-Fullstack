bcad2cb0ec583767addf7346722a8623bce2d4ec
Julien RICHARD <julien.y.richard@gmail.com>
2024-06-17 15:05:55 +0200

    Mise en place de la requete books et config du service de connexion

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,20 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import connexion from "./services/connexion";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    loader: async () => {
+      try {
+        const books = await connexion.get("/api/books");
+        return books;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
   },
 ]);
 
